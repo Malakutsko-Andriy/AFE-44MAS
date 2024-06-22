a6cf6451da996f885bd16cbf3a23d25f83eccd13
Malakutsko-Andriy <150478282+Malakutsko-Andriy@users.noreply.github.com>
2024-06-22 15:21:18 +0300

    Arrays

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,60 +1,43 @@
-function Start() {
-    const NumberA = Number(document.querySelector("#NumberA").value)
-    const NumberB = Number(document.querySelector("#NumberB").value)
-    const Actions = document.querySelector("#Action").value
-    const ResultContainer = document.getElementById("ResultContainer")
-    if (Actions == "+") {
-        let Result = (NumberA * 10 + NumberB * 10) /10
-        ResultContainer.textContent = Result
-    } else if (Actions == "-") {
-        let Result = (NumberA * 10 - NumberB * 10) /10
-        ResultContainer.textContent = Result
-    } else if (Actions == "*"){
-        let Result = ((NumberA * 10 )*(NumberB * 10)) /10
-        ResultContainer.textContent = Result
-    } else if (Actions == "/") {
-        let Result = ((NumberA * 10 )/(NumberB * 10)) /10
-        ResultContainer.textContent = Result
-    }  else if (Actions == "<") {
-        let Result = NumberA < NumberB
-        if (Result == true) {
-            ResultContainer.textContent = "Правда"
-        } else if (Result == false) {
-            ResultContainer.textContent = "Хиба"
+const GamesContainer = document.querySelector('#Games')
+const ArrayGames = ['Factorio', 'Minecraft', 'Terraria', 'Stardew Valley', 'Dead Cells']
+GamesContainer.innerHTML = 'Мої улюблені ігри це : ' + ArrayGames.join(' , ')
+let RandomNumberArray = []
+let MinNumber = 20
+let MaxNumber = 0
+const NumberContainer = document.querySelector('#ArrayNumber')
+const MinNumberContainer = document.querySelector('#MinNumber')
+const MaxNumberContainer = document.querySelector('#MaxNumber')
+let SumNumbers = 0
+let AverageValue = 0
+const AverageValueContainer = document.querySelector('#AverageValue')
+
+function RandomNumber() {
+    if (RandomNumberArray.length == 0) {
+        for (let i = 0; i < 20; i++) {
+            RandomNumberArray.push(Math.floor(Math.random() * 20) + 1)
+            if (i < 19) {
+                SumNumbers = SumNumbers + RandomNumberArray[i]
+            } else {
+                SumNumbers = SumNumbers + RandomNumberArray[i]
+                AverageValue = SumNumbers / 20
+            }
+            if (MinNumber >= RandomNumberArray[i]) {
+                MinNumber = RandomNumberArray[i]
+            }
+            if (MaxNumber <= RandomNumberArray[i]) {
+                MaxNumber = RandomNumberArray[i]
+            }
         }
-    }  else if (Actions == ">") {
-        let Result = NumberA > NumberB
-        if (Result == true) {
-            ResultContainer.textContent = "Правда"
-        } else if (Result == false) {
-            ResultContainer.textContent = "Хиба"
-        }
-    }  else if (Actions == "=") {
-        let Result = NumberA == NumberB
-        if (Result == true) {
-            ResultContainer.textContent = "Правда"
-        } else if (Result == false) {
-            ResultContainer.textContent = "Хиба"
-        }
-    }  else if (Actions == "<=") {
-        let Result = NumberA <= NumberB
-        if (Result == true) {
-            ResultContainer.textContent = "Правда"
-        } else if (Result == false) {
-            ResultContainer.textContent = "Хиба"
-        }
-    }  else if (Actions == ">=") {
-        let Result = NumberA >= NumberB
-        if (Result == true) {
-            ResultContainer.textContent = "Правда"
-        } else if (Result == false) {
-            ResultContainer.textContent = "Хиба"
-        }
-    } 
-    else {
-        alert("Error")
-        console.log(Actions)
-        console.log(NumberA)
-        console.log(NumberB)
+        NumberContainer.textContent = 'Випадкові числа  : ' + RandomNumberArray.join(' , ');
+        MinNumberContainer.textContent = 'З них най менше : ' + MinNumber
+        MaxNumberContainer.textContent = 'З них най більше : ' + MaxNumber
+        AverageValueContainer.textContent = 'Середнє значення : ' + AverageValue
+    } else {
+        RandomNumberArray = []
+        MinNumber = 20
+        MaxNumber = 0
+        SumNumbers = 0
+        AverageValue = 0
+        RandomNumber()
     }
 }
