2a5310f49ae9c1641dd7b753d0941d0179fd0631
Malakutsko-Andriy <150478282+Malakutsko-Andriy@users.noreply.github.com>
2024-07-03 15:28:50 +0300

    Cycle

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,43 +1,29 @@
-const GamesContainer = document.querySelector('#Games')
-const ArrayGames = ['Factorio', 'Minecraft', 'Terraria', 'Stardew Valley', 'Dead Cells']
-GamesContainer.innerHTML = 'Мої улюблені ігри це : ' + ArrayGames.join(' , ')
-let RandomNumberArray = []
-let MinNumber = 20
-let MaxNumber = 0
-const NumberContainer = document.querySelector('#ArrayNumber')
-const MinNumberContainer = document.querySelector('#MinNumber')
-const MaxNumberContainer = document.querySelector('#MaxNumber')
-let SumNumbers = 0
-let AverageValue = 0
-const AverageValueContainer = document.querySelector('#AverageValue')
+let Length = null
+function Start() {
+    Length = prompt('Введіть висоту ялинки')
 
-function RandomNumber() {
-    if (RandomNumberArray.length == 0) {
-        for (let i = 0; i < 20; i++) {
-            RandomNumberArray.push(Math.floor(Math.random() * 20) + 1)
-            if (i < 19) {
-                SumNumbers = SumNumbers + RandomNumberArray[i]
-            } else {
-                SumNumbers = SumNumbers + RandomNumberArray[i]
-                AverageValue = SumNumbers / 20
-            }
-            if (MinNumber >= RandomNumberArray[i]) {
-                MinNumber = RandomNumberArray[i]
-            }
-            if (MaxNumber <= RandomNumberArray[i]) {
-                MaxNumber = RandomNumberArray[i]
-            }
+    switch (Length) {
+        case ('0'): {
+            alert('Ялинка не може бути величиной 0')
+            Start()
         }
-        NumberContainer.textContent = 'Випадкові числа  : ' + RandomNumberArray.join(' , ');
-        MinNumberContainer.textContent = 'З них най менше : ' + MinNumber
-        MaxNumberContainer.textContent = 'З них най більше : ' + MaxNumber
-        AverageValueContainer.textContent = 'Середнє значення : ' + AverageValue
-    } else {
-        RandomNumberArray = []
-        MinNumber = 20
-        MaxNumber = 0
-        SumNumbers = 0
-        AverageValue = 0
-        RandomNumber()
+        case ('1'): {
+            alert('Ялинка не може бути величиной 1')
+            Start()
+        }
+    }
+    if (Length) {
+        alert('Привіт!')
+        document.querySelectorAll('div').forEach(div => {
+            div.remove()
+        })
+        for (let i = 1; i <= Length; i++) {
+            const Color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+            const Div = document.createElement('div')
+            Div.textContent = i
+            Div.style.backgroundColor = Color
+            Div.style.width = i * 10 + 'px'
+            document.body.appendChild(Div)
+        }
     }
 }
